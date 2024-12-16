88c06e1b3faf3472850084e572aa1dd5db33e4b6
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2024-12-16 14:34:29 +0100

    modify nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,9 @@ vim.opt.mouse = 'a'
 **** amsp begin ****
 ]]
 
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
 -- colorscheme of choice
 vim.cmd 'colorscheme slate'
 
@@ -16,12 +19,50 @@ vim.opt.clipboard = 'unnamedplus'
 -- ';' is easier to type than ':'
 vim.api.nvim_set_keymap("", ";", ":", {noremap = true})
 
--- hidden characters
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 --vim.opt.listchars:append {tab = "→ ",trail = "·",extends = "…",precedes = "…",nbsp = "•",space = "·",}
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- tab size
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
 -- autopairs without plugin
 --[[
