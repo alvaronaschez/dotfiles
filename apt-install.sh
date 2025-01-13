c93ef298131bd0558240402b590e0a1ee26178c9
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-01-13 14:28:19 +0100

    stuff

--- apt-install.sh
+++ apt-install.sh
@@ -1,14 +1,12 @@
-sudo apt-get update -y && sudo apt-get dist-upgrade -y
+sudo apt-get update -yq && sudo apt-get dist-upgrade -yq
 
-sudo apt-get install -y git
+sudo apt-get install -yq git
 
 # git clone https://github.com/alvaronaschez/dotfiles
 
-sudo apt-get install -y \
+sudo apt-get install -yq \
 tmux \
-# vim-gtk3 \
 stow \
-# git \
 curl \
 fonts-hack \
 foot \
@@ -21,9 +19,11 @@ qutebrowser \
 neofetch \
 zsh \
 zsh-autosuggestions \
-zsh-syntax-highligting
+zsh-syntax-highlighting
+# vim-gtk3 \
+# git \
 
-sudo apt-get install -y \
+sudo apt-get install -yq \
 gcc \
 clang-format \
 cscope \
@@ -31,36 +31,38 @@ cmake \
 universal-ctags \
 python3-pygments
 
-sudo apt-get install -y \
+sudo apt-get install -yq \
 zip \
 unzip \
 xz-utils
 
-# sudo apt-get install -y transmission-gtk
+# sudo apt-get install -yq transmission-gtk
 
-sudo apt-get install -y ripgrep less fzf bat
+sudo apt-get install -yq ripgrep less fzf bat
 
-sudo apt-get install -y waylock xdg-desktop-portal-wlr xwayland wdisplays tofi swayidle brightnessctl \
+sudo apt-get install -yq waylock xdg-desktop-portal-wlr xwayland wdisplays tofi swayidle brightnessctl \
 sway-notification-center swayimg grimshot blueman
 
 deb http://deb.debian.org/debian bookworm-backports main
 sudo apt-get update
-sudo apt-get install -y mpv yt-dlp/bookworm-backports
+sudo apt-get install -yq mpv yt-dlp/bookworm-backports
 
-sudo apt-get install -y \
+sudo apt-get install -yq \
 pulseaudio-utils \
 pavucontrol \
 pamixer \
 net-tools
 
-sudo apt-get install -y vlc obs-studio evince evince-previewer nautilus
+sudo apt-get install -yq vlc obs-studio evince evince-previewer nautilus
 
-sudo apt-get install -y luarocks
+sudo apt-get install -yq luarocks
 
-sudo apt-get install -y \
+sudo apt-get install -yq \
 qemu-system \
 libvirt-daemon-system \
 virt-manager
+
+chsh -s $(which zsh)
 
 ## install docker following official website instructions
 # docker-desktop.deb
