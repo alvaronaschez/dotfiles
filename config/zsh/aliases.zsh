613929e0977a389d42e4ecd1577359a1e16a1302
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-02-03 14:37:28 +0100

    stuff

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -89,6 +89,7 @@ alias nvranger="nv ~/.config/ranger/rifle.conf"
 alias nvneofetch="nv ~/.config/neofetch/config.conf"
 alias nvscratch="nv ~/scratchpad.txt"
 alias nvtofi="nv ~/.config/tofi/config"
+alias vvieb="v ~/.config/Vieb/viebrc"
 
 alias szsh="source ~/.zshrc"
 alias stmux="tmux source ~/.config/tmux/tmux.conf"
